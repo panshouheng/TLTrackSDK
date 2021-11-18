cc02c7bab22aae36d1e1930719700214f1aea9e1
潘守恒 <shouheng.pan@tineco.com>
2021-11-18 13:02:11 +0800

    update version

--- Example/Pods/Target Support Files/TLTrackSDK/TLTrackSDK-umbrella.h
+++ Example/Pods/Target Support Files/TLTrackSDK/TLTrackSDK-umbrella.h
@@ -12,23 +12,6 @@
 
 #import "NSObject+SASwizzler.h"
 #import "SensorsAnalyticsDatabase.h"
-#import "SensorsAnalyticsDelegateProxy.h"
-#import "SensorsAnalyticsDynamicDelegate.h"
-#import "SensorsAnalyticsExceptionHandler.h"
-#import "SensorsAnalyticsExtensionDataManager.h"
-#import "SensorsAnalyticsFileStore.h"
-#import "SensorsAnalyticsKeychainItem.h"
-#import "SensorsAnalyticsNetwork.h"
-#import "SensorsAnalyticsSDK.h"
-#import "SensorsSDK.h"
-#import "UIApplication+SensorsData.h"
-#import "UICollectionView+SensorsData.h"
-#import "UIControl+SensorsData.h"
-#import "UIGestureRecognizer+SensorsData.h"
-#import "UIScrollView+SensorsData.h"
-#import "UITableView+SensorsData.h"
-#import "UIView+SensorsData.h"
-#import "UIViewController+SensorsData.h"
 
 FOUNDATION_EXPORT double TLTrackSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char TLTrackSDKVersionString[];
